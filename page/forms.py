df25bc5a1beca0835d6032110969fcf4521185c3
mathengejoan <mathengejoan971@gmail.com>
2019-05-15 14:53:00 +0300

    happy

---
+++ page/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Album, Song
+from django.contrib.auth.models import User
+
+
+class AlbumForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = ['name', 'artist', 'cover', 'genre', 'year']
+
+
+class SongForm(forms.ModelForm):
+    class Meta:
+        model = Song
+        fields = ['name', 'audio']
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
---
+++ page/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'page'
+urlpatterns =[
+    path('',views.index,name='index'),
+    path('(?P<album_id>[0-9]+/)', views.details,name='details'),
+    path('add_album',views.add_album,name='add_album'),
+    path('(?P<album_id>[0-9]+)/add_song', views.add_song, name='add_song'),
+    path('(?P<album_id>[0-9]+)/delete-album', views.delete_album, name='delete-album'),
+    path('signup',views.signup, name='signup'),
+    #path('login', auth_views.LoginView.as_view(), name='login'),
+    #path('logout',auth_views.LogoutView.as_view(), name='logout'),
+    path('login',views.login_user,name='login'),
+    path('logout',views.logout_user,name='logout'),
+
+]
